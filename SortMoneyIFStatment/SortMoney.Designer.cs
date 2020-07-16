a9e66bbe94614a47910ea58c860222e2dc9faf12
cdup <cdup@MSI>
2020-07-16 15:48:54 +1200

    Add project files.

---
+++ SortMoneyIFStatment/SortMoney.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace SortMoneyIFStatment
+{
+    partial class SortMoney
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSortMoney = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbxMoney100 = new System.Windows.Forms.TextBox();
+            this.tbxMoney50 = new System.Windows.Forms.TextBox();
+            this.tbxMoney20 = new System.Windows.Forms.TextBox();
+            this.tbxMoney10 = new System.Windows.Forms.TextBox();
+            this.tbxMoney5 = new System.Windows.Forms.TextBox();
+            this.tbxMoney2 = new System.Windows.Forms.TextBox();
+            this.tbxMoney1 = new System.Windows.Forms.TextBox();
+            this.tbxEnterValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSortMoney
+            // 
+            this.btnSortMoney.Location = new System.Drawing.Point(304, 107);
+            this.btnSortMoney.Name = "btnSortMoney";
+            this.btnSortMoney.Size = new System.Drawing.Size(116, 49);
+            this.btnSortMoney.TabIndex = 0;
+            this.btnSortMoney.Text = "Sort Money";
+            this.btnSortMoney.UseVisualStyleBackColor = true;
+            this.btnSortMoney.Click += new System.EventHandler(this.btnSortMoney_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "20";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(547, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(671, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(301, 369);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(133, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Money";
+            // 
+            // tbxMoney100
+            // 
+            this.tbxMoney100.Location = new System.Drawing.Point(12, 301);
+            this.tbxMoney100.Name = "tbxMoney100";
+            this.tbxMoney100.Size = new System.Drawing.Size(100, 22);
+            this.tbxMoney100.TabIndex = 9;
+            // 
+            // tbxMoney50
+            // 
+            this.tbxMoney50.Location = new System.Drawing.Point(136, 301);
+            this.tbxMoney50.Name = "tbxMoney50";
+            this.tbxMoney50.Size = new System.Drawing.Size(100, 22);
+            this.tbxMoney50.TabIndex = 10;
+            // 
+            // tbxMoney20
+            // 
+            this.tbxMoney20.Location = new System.Drawing.Point(260, 301);
+            this.tbxMoney20.Name = "tbxMoney20";
+            this.tbxMoney20.Size = new System.Drawing.Size(100, 22);
+            this.tbxMoney20.TabIndex = 11;
+            // 
+            // tbxMoney10
+            // 
+            this.tbxMoney10.Location = new System.Drawing.Point(384, 301);
+            this.tbxMoney10.Name = "tbxMoney10";
+            this.tbxMoney10.Size = new System.Drawing.Size(100, 22);
+            this.tbxMoney10.TabIndex = 12;
+            // 
+            // tbxMoney5
+            // 
+            this.tbxMoney5.Location = new System.Drawing.Point(506, 301);
+            this.tbxMoney5.Name = "tbxMoney5";
+            this.tbxMoney5.Size = new System.Drawing.Size(100, 22);
+            this.tbxMoney5.TabIndex = 13;
+            // 
+            // tbxMoney2
+            // 
+            this.tbxMoney2.Location = new System.Drawing.Point(630, 301);
+            this.tbxMoney2.Name = "tbxMoney2";
+            this.tbxMoney2.Size = new System.Drawing.Size(100, 22);
+            this.tbxMoney2.TabIndex = 14;
+            // 
+            // tbxMoney1
+            // 
+            this.tbxMoney1.Location = new System.Drawing.Point(260, 400);
+            this.tbxMoney1.Name = "tbxMoney1";
+            this.tbxMoney1.Size = new System.Drawing.Size(100, 22);
+            this.tbxMoney1.TabIndex = 15;
+            // 
+            // tbxEnterValue
+            // 
+            this.tbxEnterValue.Location = new System.Drawing.Point(200, 35);
+            this.tbxEnterValue.Name = "tbxEnterValue";
+            this.tbxEnterValue.Size = new System.Drawing.Size(323, 22);
+            this.tbxEnterValue.TabIndex = 16;
+            // 
+            // SortMoney
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbxEnterValue);
+            this.Controls.Add(this.tbxMoney1);
+            this.Controls.Add(this.tbxMoney2);
+            this.Controls.Add(this.tbxMoney5);
+            this.Controls.Add(this.tbxMoney10);
+            this.Controls.Add(this.tbxMoney20);
+            this.Controls.Add(this.tbxMoney50);
+            this.Controls.Add(this.tbxMoney100);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSortMoney);
+            this.Name = "SortMoney";
+            this.Text = "Sort Money";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSortMoney;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbxMoney100;
+        private System.Windows.Forms.TextBox tbxMoney50;
+        private System.Windows.Forms.TextBox tbxMoney20;
+        private System.Windows.Forms.TextBox tbxMoney10;
+        private System.Windows.Forms.TextBox tbxMoney5;
+        private System.Windows.Forms.TextBox tbxMoney2;
+        private System.Windows.Forms.TextBox tbxMoney1;
+        private System.Windows.Forms.TextBox tbxEnterValue;
+    }
+}
+
---
+++ SortMoneyIFStatment/SortMoney.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortMoneyIFStatment
+{
+    public partial class SortMoney : Form
+    {
+        public SortMoney()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSortMoney_Click(object sender, EventArgs e)
+        {
+
+            // use the textbox value (input value ) to sort the money
+
+            int num100 = Convert.ToInt32(tbxEnterValue.Text) / 100;
+            tbxMoney100.Text = num100.ToString();
+
+            if (num100 != 0) // check if there is any hundreds 
+            {
+                // work out how many hundreds are there
+                int t = num100 * 100;
+
+                t = Convert.ToInt32(tbxEnterValue.Text) - t;
+
+                tbxEnterValue.Text = t.ToString();
+            }
+
+            int num50 = Convert.ToInt32(tbxEnterValue.Text) / 50;
+            tbxMoney50.Text = num50.ToString();
+
+
+            if (num50 != 0)// check if there is any Fifties
+            {
+                // work out how many fifties are there
+                int t = num50 * 50;
+
+                t = Convert.ToInt32(tbxEnterValue.Text) - t;
+
+                tbxEnterValue.Text = t.ToString();
+            }
+
+            int num20 = Convert.ToInt32(tbxEnterValue.Text) / 20;
+            tbxMoney20.Text = num20.ToString();
+
+            if (num20 != 0)// check if there is any twenties 
+            {
+                // work out how many twenties are there
+                int t = num20 * 20;
+
+                t = Convert.ToInt32(tbxEnterValue.Text) - t;
+
+                tbxEnterValue.Text = t.ToString();
+            }
+
+            int num10 = Convert.ToInt32(tbxEnterValue.Text) / 10;
+            tbxMoney10.Text = num10.ToString();
+
+            if (num10 != 0)// check if there is any tens 
+            {
+                // work out how many tens are there
+                int t = num10 * 10;
+
+                t = Convert.ToInt32(tbxEnterValue.Text) - t;
+
+                tbxEnterValue.Text = t.ToString();
+            }
+
+            int num5 = Convert.ToInt32(tbxEnterValue.Text) / 5;
+            tbxMoney5.Text = num5.ToString();
+
+            if (num5 != 0)// check if there is any fives 
+            {
+                // work out how many fives are there
+                int t = num5 * 5;
+
+                t = Convert.ToInt32(tbxEnterValue.Text) - t;
+
+                tbxEnterValue.Text = t.ToString();
+            }
+
+
+            int num2 = Convert.ToInt32(tbxEnterValue.Text) / 2;
+            tbxMoney2.Text = num2.ToString();
+
+            if (num2 != 0)// check if there is any twos 
+            {
+                // work out how many twos are there
+                int t = num2 * 2;
+
+                t = Convert.ToInt32(tbxEnterValue.Text) - t;
+
+                tbxEnterValue.Text = t.ToString();
+            }
+
+            int num1 = Convert.ToInt32(tbxEnterValue.Text) / 1;
+            tbxMoney1.Text = num1.ToString();
+
+            if (num1 != 0)// check if there is any ones
+            {
+                // work out how many ones are there
+                int t = num1 * 1;
+
+                t = Convert.ToInt32(tbxEnterValue.Text) - t;
+
+                tbxEnterValue.Text = t.ToString();
+            }
+
+        }
+    }
+}
